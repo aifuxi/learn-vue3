a69e1d13ea39070fbf48ebd131df6161424a5a02
aifuxi <aifuxi.js@gmail.com>
2023-08-03 01:52:01 +0800

    feat📦: 学习toXX全家桶

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import FirstVue from "./components/FirstVue.vue";
 import LearnReactive from "./components/LearnReactive.vue";
+import LearnToX from "./components/LearnToX.vue";
 </script>
 
 <template>
   <FirstVue />
   <LearnReactive />
+  <LearnToX />
 </template>
---
+++ src/components/LearnToX.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <hr />
+    {{ p }}
+
+    <br />
+
+    <code>{{ name }}</code> -- <code>{{ age }}</code>
+
+    <button type="button" @click="handleClick">修改</button>
+
+    <h3>{{ newName }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, toRefs, toRaw, onMounted, toRef } from "vue";
+
+const p = reactive({ name: "gg", age: 20 });
+
+// 如果想要解构reactive包装的值，需要使用toRefs
+const { name, age } = toRefs(p);
+
+const newName = toRef(p, "name");
+
+onMounted(() => {
+  // toRaw 返回原始值
+  console.log(toRaw(p));
+});
+
+function handleClick() {
+  newName.value = "我是新的name";
+}
+</script>
+
+<style scoped></style>
